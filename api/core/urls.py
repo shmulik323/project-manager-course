6b7cec141e10e0d6e7e1533b1a00fbff7ce35e15
alexv32 <alexvaitzman@gmail.com>
2019-03-22 18:19:44 +0200

    changes to backend that works

--- api/core/urls.py
+++ api/core/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 
 router = DefaultRouter()
-router.register(r'recipes', UserViewSet)
+router.register(r'user', UserViewSet ,base_name='user')
 
 urlpatterns = [
     path("", include(router.urls))
