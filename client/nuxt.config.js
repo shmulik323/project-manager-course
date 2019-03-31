8d674cb22c805223e9046c5b6b65d5c892190353
shmulik323 <shmulik323@gmail.com>
2019-03-31 18:56:05 +0300

    replased backend to flask and add google auth

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -31,7 +31,7 @@ module.exports = {
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Material+Icons'
       }, {
         rel: 'stylesheet',
         href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'
@@ -66,37 +66,46 @@ module.exports = {
   modules: [
     '@nuxtjs/axios',
     '@nuxtjs/auth',
-    '@nuxtjs/proxy'
+    '@nuxtjs/proxy',
+    'bootstrap-vue/nuxt'
   ],
   axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-    baseURL: 'http://127.0.0.1:3333/api'
+    baseURL: 'http://127.0.0.1:5000',
+    proxy: true
   },
-  proxy: [
-    ['/patentapi', {
-      target: process.env.PROXY_PLACE_API_URL || 'https://developer.uspto.gov/ibd-api/v1/patent/application',
-      pathRewrite: {
-        '^/patentapi': ''
-      }
-    }],
-
-  ],
+  proxy: {
+    '/api': 'http://127.0.0.1:5000',
+    '/api/auth/user': 'http://127.0.0.1:5000',
+    '/api/auth/login': 'http://127.0.0.1:5000'
+  },
   auth: {
+    redirect: {
+      callback: '/callback'
+    },
     strategies: {
       local: {
         endpoints: {
           login: {
-            url: 'login',
-            method: 'post',
-            propertyName: 'data.token'
-          },
-          user: {
-            url: 'me',
             method: 'get',
-            propertyName: 'data'
-          },
-          logout: false
+            propertyName: 'token'
+          }
         }
+      },
+      auth0: {
+        domain: 'nuxt-auth.auth0.com',
+        client_id: 'q8lDHfBLJ-Fsziu7bf351OcYQAIe3UJv'
+      },
+      facebook: {
+        client_id: '1671464192946675',
+        userinfo_endpoint: 'https://graph.facebook.com/v2.12/me?fields=about,name,picture{url},email,birthday',
+        scope: ['public_profile', 'email', 'user_birthday']
+      },
+      google: {
+        client_id: '956748748298-kr2t08kdbjq3ke18m3vkl6k843mra1cg.apps.googleusercontent.com'
+      },
+
+      twitter: {
+        client_id: 'FAJNuxjMTicff6ciDKLiZ4t0D'
       }
     }
   },
